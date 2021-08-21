a0114970d27e03dc21e2fcc1866bab9d8bd5960d
vk0808 <vk4765712@gmail.com>
2021-08-21 23:46:52 +0530

    add: validate password rule3

--- UserRegistration/FormValidate.cs
+++ UserRegistration/FormValidate.cs
@@ -8,10 +8,10 @@ namespace UserRegistration
     public class FormValidate
     {
         // Patterns
-        public string REGEX_NAME = "^[A-Z]{1}[A-Za-z]{2,}";
+        public string REGEX_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         public string REGEX_EMAIL = @"^[A-Za-z0-9]+([\.+\-_][A-Za-z0-9]+)*@[a-zA-Z0-9]+\.?[A-Za-z]+\.?[A-Za-z]{2,}$";
         public string REGEX_MOBILE = "^[1-9]{1}[0-9]{1}\\s[1-9]{1}[0-9]{9}$";
-        public string REGEX_PASSWORD = "(?=.*[A-Z])[a-zA-Z0-9]{8,}$";
+        public string REGEX_PASSWORD = "(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$";
 
 
         // Default form validator
