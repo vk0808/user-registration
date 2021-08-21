62b682fd8d754e1b666c90e2a1535a3bf5e945f3
vk0808 <vk4765712@gmail.com>
2021-08-21 16:41:38 +0530

    add: welcome message

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration\n");
+        }
+    }
+}
