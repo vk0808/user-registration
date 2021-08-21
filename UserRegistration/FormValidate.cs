369252ae89aa46113a4239c6cb529925a5ea6602
vk0808 <vk4765712@gmail.com>
2021-08-21 22:41:08 +0530

    add: validate firstname

---
+++ UserRegistration/FormValidate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class FormValidate
+    {
+        // Patterns
+        public string REGEX_NAME = "^[A-Z]{1}[A-Za-z]{2,}";
+
+
+        // Default form validator
+        public void Form(string field, string REGEX)
+        {
+            bool flag = false;
+            Console.WriteLine($"Enter the {field}");
+
+            while (!flag)
+            {
+                string field_value = Console.ReadLine();
+                flag = (Regex.IsMatch(field_value, REGEX));
+                if (flag == true)
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid, type again");
+                }
+            }
+        }
+    }
+}
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,7 +6,14 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
+            // Welcome message
             Console.WriteLine("User Registration\n");
+
+            // Object instantisation
+            FormValidate form = new FormValidate();
+
+            
+            form.Form("First Name", form.REGEX_NAME);
         }
     }
 }
